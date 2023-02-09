cd041bd63f295cf32b06afb51da706b7ed9e785e
7007singh <shw12021982@gmail.com>
2023-02-09 16:21:31 +0530

    [Shweta]Add: Welcome to Address book Program

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public void Person(string firstName, string lastName, string address, string city, string state, int zip, int phoneNumber, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
+    }
+}
